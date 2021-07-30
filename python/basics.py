f5a2b08e5e9ae8294b9d8df003a1664b4b447e50
rose-is-gay <71583406+rose-is-gay@users.noreply.github.com>
2021-07-30 04:31:41 -0400

    Add files via upload

---
+++ python/basics.py
@@ -0,0 +1,12 @@
+if True:
+	print ("h")
+	print("asd")
+
+# fuck
+
+name = "fuck me"
+num1 = 5; num2 = 2
+
+long_name = "something" + "e"
+
+print ("penis")
---
+++ python/sn.py
@@ -0,0 +1,17 @@
+# single line 
+text = "hello"
+# multi line
+multi_line = """line1"""
+# substrings
+name = "engineer man"
+first_name = name[0:8]
+# integer
+num1 = 6
+# float
+dec1 = 5.4
+# conversion to string
+to_string = str (sum1)
+# conversion to integer/float
+num_string = "5"
+to_int = int(num_string)
+to_float = float(num_string)
